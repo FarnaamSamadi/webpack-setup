34f3f5e61789e3ec3a10c3cf948ea1e6d65c16cf
farnaam <fm.samadi70@gmail.com>
2022-03-26 16:02:47 +0430

    Add html and css module handling.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import "@/styles/index.scss";
+
 const sayHello = () => {
   console.log("Hello!");
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,39 @@
 const { resolve } = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "production",
 
-  entry: "./src/main.js",
+  entry: {
+    main: resolve(__dirname, "src/main.js"),
+  },
 
   output: {
     path: resolve(__dirname, "dist"),
     filename: "[name]-[contenthash].js",
     clean: true,
   },
+
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "My app",
+      filename: "index.html",
+      template: "public/index.html",
+    }),
+  ],
 };
